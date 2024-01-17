617bf5066838702e495f485d82d27aa52a700dd2
SAT3A <94332097+SAT3A@users.noreply.github.com>
2024-01-17 22:33:29 +0700

    basic syntax

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        String fname = "irvansah", lname = "Satria";
+        System.out.println(fname + " " + lname );
+
+        int age = 24, score = 95;
+        float tb = 178.5f;
+
+        float num1 = 19.5f;
+        int num2 = (int)num1;
+
+        System.out.println("hello " + "my name is " + fname + " " + lname + " \n my age is " + age + "and my height " + tb);
+        System.out.println("float 19.5 to int = " + num2 );
+
+        if (age > 18){
+            System.out.println("Kamu bisa memilih");
+
+        }else if (age >= 17){
+            System.out.println("Kamu harus memiliki kartu identitas terlebih dahulu");
+        }else{
+            System.out.println("Kamu belum bisa memilih");
+        }
+
+        //shorthand if-else
+        String result = (score <= 75 ) ?  "Tidak bisa melanjutkan" : "Silahkan lanjut ke step berikutnya";
+        System.out.println(result);
 
 
+        //while loops
+
+        int start = 0;
+        while (start <= 10){
+            System.out.println("angka " + start);
+            start++;
+        }
+
+        // do while, kalo ini dilakuin dulu baru di cek
+        int mulai = 0;
+        do{
+            System.out.println("number " + mulai);
+            mulai++;
+        }while(mulai<=10);
+
+        //for loops
+        for (int dari = 1; dari<=5; dari++){
+            System.out.println("ini akan di print sebanyak " + dari + " kali");
+        }
+
+        //for each
+
+        String[] friend = {"Dennis","James","Rose"};
+        for (String i: friend){
+            System.out.println("our friends is " + i);
+        }
+
+        String[] fruit = {"Banana","Apple","Pear"};
+        for (int a = 0; a<= fruit.length; a++){
+            System.out.println(fruit[a]);
+        }
     }
 }
